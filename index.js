d0ff202530925cfd89eacae8d3f5634396e0b2fb
Christian Fei <crifei93@gmail.com>
2020-02-15 16:31:11 +0100

    add getTwitterUserProfile

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const OAuth = require('oauth')
+const { promisify } = require('util')
+
+if (require.main === module) {
+  getTwitterUserProfile('twitterdev')
+    .then((profile) => console.log(JSON.stringify(profile, null, 2)) && process.exit(0))
+    .catch(err => console.error(err) && process.exit(1))
+} else {
+  module.exports = { getTwitterUserProfile }
+}
+
+async function getTwitterUserProfile (username = 'twitterdev') {
+  var oauth = new OAuth.OAuth(
+    'https://api.twitter.com/oauth/request_token',
+    'https://api.twitter.com/oauth/access_token',
+    process.env.TWITTER_CONSUMER_KEY,
+    process.env.TWITTER_CONSUMER_SECRET,
+    '1.0A', null, 'HMAC-SHA1'
+  )
+  const get = promisify(oauth.get.bind(oauth))
+
+  const body = await get(
+    `https://api.twitter.com/1.1/users/show.json?screen_name=${username}`,
+    process.env.TWITTER_ACCESS_KEY,
+    process.env.TWITTER_ACCESS_TOKEN_SECRET
+  )
+
+  return JSON.parse(body)
+}
